ef81b79d699d9b8c69a64e48b90e6e5d5367f11d
Rowan <104677625+rowandevving@users.noreply.github.com>
2024-01-20 16:19:42 +0000

    Add tray menu and autostart toggle

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.20
 
 require github.com/danieloliveira085/autostarter v1.1.0
 
+require github.com/pelletier/go-toml/v2 v2.1.1 // direct
+
 require (
 	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/mattn/go-pointer v0.0.1 // indirect
+	github.com/trevex/tray v0.0.0-20201029085003-af20c8c6df9f // direct
 	golang.org/x/sys v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,31 +4,115 @@ Copyright © 2024 rowandev
 package main
 
 import (
-	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/danieloliveira085/autostarter"
+	"github.com/pelletier/go-toml/v2"
+	"github.com/trevex/tray"
 )
 
-func main() {
-	//detect if this is the first time the app has been ran
-	if _, err := os.Stat(os.Getenv("APPDATA") + "/scape.exists"); os.IsNotExist(err) {
-		// add the app to startup apps
-		fmt.Println("First-time install detected, adding to startup apps...")
-		exec, err := os.Executable()
-		if err != nil {
-			fmt.Println("Error getting executable path, Tip: Make sure it's not symlinked. Error:", err)
-		}
-		app := autostarter.NewAutostart(
-			autostarter.Shortcut{
-				Name: "Scape",
-				Exec: exec,
-				Args: []string{},
-			},
-			autostarter.DefaultIcon,
-		)
-		app.Enable()
+type Config struct {
+	runOnStartup bool
+}
 
-		os.Create(os.Getenv("APPDATA") + "/scape.exists")
+func main() {
+	var configDir string
+	var autostart *autostarter.Autostart
+
+	if runtime.GOOS == "windows" {
+		configDir = os.Getenv("APPDATA") + "/scape.toml"
+		//detect if this is the first time the app has been ran
+		if _, err := os.Stat(configDir); os.IsNotExist(err) {
+			// add the app to startup apps
+			exec, err := os.Executable()
+			if err != nil {
+				panic(err)
+			}
+			autostart = autostarter.NewAutostart(
+				autostarter.Shortcut{
+					Name: "Scape",
+					Exec: exec,
+					Args: []string{},
+				},
+				autostarter.DefaultIcon,
+			)
+			autostart.Enable()
+
+			os.Create(configDir)
+		}
+	} else if runtime.GOOS == "linux" {
+		configDir = os.Getenv("HOME") + "/scape.toml"
+		//detect if this is the first time the app has been ran
+		if _, err := os.Stat(configDir); os.IsNotExist(err) {
+			// add the app to startup apps
+			exec, err := os.Executable()
+			if err != nil {
+				os.Exit(1)
+			}
+			autostart = autostarter.NewAutostart(
+				autostarter.Shortcut{
+					Name: "Scape",
+					Exec: exec,
+					Args: []string{},
+				},
+				autostarter.DefaultIcon,
+			)
+			autostart.Enable()
+
+			os.Create(configDir)
+		}
+	} else if runtime.GOOS == "darwin" {
+		configDir = os.Getenv("HOME") + "/Library/Application Support/scape.toml"
+		// add the app to startup apps
+		if _, err := os.Stat(configDir); os.IsNotExist(err) {
+			// add the app to startup apps
+			exec, err := os.Executable()
+			if err != nil {
+				os.Exit(1)
+			}
+			autostart = autostarter.NewAutostart(
+				autostarter.Shortcut{
+					Name: "Scape",
+					Exec: exec,
+					Args: []string{},
+				},
+				autostarter.DefaultIcon,
+			)
+			autostart.Enable()
+
+			os.Create(configDir)
+		}
 	}
+
+	appTray := tray.Tray{
+		Menu: []*tray.Menu{
+			tray.Seperator,
+			{
+				Text: "Run on startup",
+				Callback: func(appTray *tray.Tray, menu *tray.Menu) {
+					cfgFile, err := os.ReadFile(configDir)
+					if err != nil {
+						panic(err)
+					}
+					var cfg Config
+					err = toml.Unmarshal([]byte(cfgFile), &cfg)
+					if err != nil {
+						panic(err)
+					}
+					if cfg.runOnStartup {
+						cfg.runOnStartup = false
+						autostart.Disable()
+						menu.Checked = false
+					} else {
+						cfg.runOnStartup = true
+						autostart.Enable()
+						menu.Checked = true
+					}
+				},
+			},
+		},
+	}
+	appTray.Run()
+
 }
